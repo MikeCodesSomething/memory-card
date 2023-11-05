a9246d374da93c309d5c2cca5501fbca980e05e2
Mike Williams <MichaelJamesWilliams193@gmail.com>
2023-11-05 17:18:45 +0000

    Made shuffle run 3 times instead of once as it didn't seem to be getting very shuffled

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -77,10 +77,15 @@ export default function Gameboard() {
   }
 
   function shuffle<T>(array: T[]): T[] {
+    //shuffle 3 times cause once was not changing it enough
+    for (let i = 0; i < 2; i++)
+    {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
+  }
+    
     return array;
   }
 
